7b86afd7fc01c178e902342e59e6208e311bed8e
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-02 07:39:56 -0500

    Update first-unique-character-in-a-string.py

--- first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string.py
@@ -1,17 +1,16 @@
+"""
+387. First Unique Character in a String
+https://leetcode.com/problems/first-unique-character-in-a-string/
+"""
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        if s == "": return -1
-        
-        d = {}
-        for i, char in enumerate(s):
-            if char not in d:
-                d[char] = [i]
-            else:
-                d[char].append(i)
-        
-        for char in d:
-            if len(d[char]) == 1:
-                return d[char][0]
-        
-        return -1
-        
+    def firstUniqChar(self, s: str) -> int:
+        d = {}
+        for i, char in enumerate(s):
+            if char not in d: d[char] = [i]
+            else: d[char].append(i)
+        
+        for char in d:
+            if len(d[char]) == 1: return d[char][0]
+        
+        return -1
+       
