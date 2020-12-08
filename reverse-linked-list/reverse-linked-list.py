7cd3b3463cbffb6d8830179a277b082cbc42274e
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-07 19:10:17 -0500

    Update reverse-linked-list.py

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -4,19 +4,12 @@ https://leetcode.com/problems/reverse-linked-list/
 """
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        if not head: return None
-        curr = head
-        curr_prev, curr_next = None, head.next
-        while curr_next:
-            curr.next = curr_prev
-            curr_prev = curr
-            curr = curr_next
-            curr_next = curr.next
-        curr.next = curr_prev
-        head = curr
-        return head
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
