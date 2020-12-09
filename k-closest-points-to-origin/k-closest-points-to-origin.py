f86feb382b1a00466c8540bd6f6c434d1c1b87ba
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-09 08:41:50 -0500

    Update k-closest-points-to-origin.py

--- k-closest-points-to-origin/k-closest-points-to-origin.py
+++ k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,26 +1,22 @@
 class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        d_to_point, p_distances = {}, []
-        for point in points:
-            distance = distance_squared(point)
-            if distance not in d_to_point:
-                d_to_point[distance] = [point]
-                p_distances.append(distance)
-            else: 
-                d_to_point[distance].append(point)
-        
-        # now retrieve the top K distances
-        p_distances.sort()
-        return_l = []
-        for distance in p_distances:
-            print(distance)
-            if len(d_to_point[distance]) >= K:
-                return return_l + d_to_point[distance][:K]
-            else:
-                return_l += d_to_point[distance]
-                K -= len(d_to_point[distance])
-        return return_l
-​
-    
-def distance_squared(p) -> int:
-    return p[0]**2 + p[1]**2
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        d_to_point, p_distances = {}, []
+        for point in points:
+            distance = point[0]**2 + point[1]**2
+            if distance not in d_to_point:
+                d_to_point[distance] = [point]
+                p_distances.append(distance)
+            else: 
+                d_to_point[distance].append(point)
+        
+        # now retrieve the top K distances
+        p_distances.sort()
+        return_l = []
+        for distance in p_distances:
+            curr_points_in_distance = len(d_to_point[distance])
+            if curr_points_in_distance >= K:
+                return return_l + d_to_point[distance][:K]
+            else:
+                return_l += d_to_point[distance]
+                K -= curr_points_in_distance
+        return return_l
