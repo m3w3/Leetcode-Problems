c20410962808f418781f0c63c80da967ca974417
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-09 09:40:29 -0500

    Update top-k-frequent-words.py

--- top-k-frequent-words/top-k-frequent-words.py
+++ top-k-frequent-words/top-k-frequent-words.py
@@ -5,45 +5,32 @@ https://leetcode.com/problems/top-k-frequent-words/
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        d, l_ = {}, [] # word -> frequency, return list
+        freq_words, frequencies = {}, set()
+        word_freq = word_frequencies(words)
         for word in words:
-            if word not in d: d[word] = 1
-            else: d[word] += 1
-        # O(n)
-        d_reversed, counts_desc = reversed_counts(d) # <- helper below
-        # O(n) + O(n log n)
-        for count in counts_desc: # visit count in descending order of counts
-            curr_word_list = d_reversed[count]
-            length_of_list = len(curr_word_list)
-            if length_of_list <= k:
-                l_ += curr_word_list
-                k -= length_of_list
-            else:
-                l_ += curr_word_list[:k]
-                break
-        # O(n log n)
-        return l_
+            curr_freq = word_freq[word]
+            if curr_freq not in freq_words: 
+                freq_words[curr_freq] = {word}
+                frequencies.add(curr_freq)
+            else: 
+                freq_words[curr_freq].add(word)
 
-def reversed_counts(d):
-    """
-    >>> d = {'d': 2, 'c': 2, 'b': 1, 'a': 1}
-    >>> reversed_counts(d, words)
-    {2: ['c', 'd'], 1: ['a', 'b']}, [2, 1]
-    """
-    d_reversed = {} # frequency -> list of word(s)
-    counts = [] # list of frequency counts
-    for word in d:
-        word_count = d[word]
-        if word_count not in d_reversed:
-            d_reversed[word_count] = [word]
-            counts.append(word_count)
-        else: d_reversed[word_count].append(word)
-    # O(n)
-    
-    # extra stuff below: also sort them here
-    for count in d_reversed:
-        d_reversed[count].sort()
-    # O(n)
-    counts.sort()
-    # O(n log n)
-    return d_reversed, counts[::-1]
+        return_l = []
+        frequencies = sorted(list(frequencies), reverse=True)
+        
+        for freq in frequencies:
+            curr_words_len = len(freq_words[freq])
+            freq_words[freq] = sorted(list(freq_words[freq]))
+            if curr_words_len >= k:
+                return return_l + freq_words[freq][:k]
+            else:
+                return_l += freq_words[freq]
+                k -= curr_words_len
+        return return_l
+
+def word_frequencies(words: List[str]) -> dict:
+    d = {}
+    for word in words:
+        if word not in d: d[word] = 1
+        else: d[word] += 1
+    return d
