4be4a44e924e3b72ecbcfedfad6e392bccdc39e9
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-09 21:06:54 -0500

    working commit - Created using LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -12,24 +12,22 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         head = ListNode()
         curr = head
-        curr_l1, curr_l2 = l1, l2
-        while curr_l1 and curr_l2:
-            if curr_l1.val < curr_l2.val: 
-                curr.next = ListNode(val=curr_l1.val)
-                curr, curr_l1 = curr.next, curr_l1.next
+        while l1 and l2:
+            if l1.val < l2.val: 
+                curr.next = l1
+                curr, l1 = curr.next, l1.next
             else: 
-                curr.next = ListNode(val=curr_l2.val)
-                curr, curr_l2 = curr.next, curr_l2.next
-                
+                curr.next = l2
+                curr, l2 = curr.next, l2.next
                 
         # hit end of one of the two (maybe both) linked lists
-        # append rest of l2
-        while curr_l2:
-            curr.next = ListNode(val=curr_l2.val)
-            curr, curr_l2 = curr.next, curr_l2.next
         # append rest of l1
-        while curr_l1:
-            curr.next = ListNode(val=curr_l1.val)
-            curr, curr_l1 = curr.next, curr_l1.next
+        while l1:
+            curr.next = l1
+            curr, l1 = curr.next, l1.next
+        # append rest of l2
+        while l2:
+            curr.next = l2
+            curr, l2 = curr.next, l2.next
         
         return head.next
