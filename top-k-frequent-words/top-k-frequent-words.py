0f6cacd9f7fe06736ed151744a5f4529441d88cf
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2020-12-25 21:26:11 -0500

    Update top-k-frequent-words.py

--- top-k-frequent-words/top-k-frequent-words.py
+++ top-k-frequent-words/top-k-frequent-words.py
@@ -2,35 +2,23 @@
 692. Top K Frequent Words
 https://leetcode.com/problems/top-k-frequent-words/
 """
+from heapq import *
+class Helper:
+    def __init__(self, word, word_freq):
+        self.word, self.word_freq = word, word_freq
+    def __lt__(self, Helper2): # used to compare if self < Helper2
+        if self.word_freq == Helper2.word_freq: 
+            return self.word > Helper2.word
+        return self.word_freq < Helper2.word_freq
+    def __eq__(self, Helper2):
+        return self.word == Helper2.word and self.word_freq == Helper2.word_freq
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freq_words, frequencies = {}, set()
-        word_freq = word_frequencies(words)
-        for word in words:
-            curr_freq = word_freq[word]
-            if curr_freq not in freq_words: 
-                freq_words[curr_freq] = {word}
-                frequencies.add(curr_freq)
-            else: 
-                freq_words[curr_freq].add(word)
-
-        return_l = []
-        frequencies = sorted(list(frequencies), reverse=True)
-        
-        for freq in frequencies:
-            curr_words_len = len(freq_words[freq])
-            freq_words[freq] = sorted(list(freq_words[freq]))
-            if curr_words_len >= k:
-                return return_l + freq_words[freq][:k]
-            else:
-                return_l += freq_words[freq]
-                k -= curr_words_len
-        return return_l
-
-def word_frequencies(words: List[str]) -> dict:
-    d = {}
-    for word in words:
-        if word not in d: d[word] = 1
-        else: d[word] += 1
-    return d
+        frequency = Counter(words)
+        min_heap = []
+        for word, freq in frequency.items():
+            heappush(min_heap, Helper(word, freq))
+            if len(min_heap) > k: 
+                heappop(min_heap)
+        return [heappop(min_heap).word for _ in range(len(min_heap))][::-1]
