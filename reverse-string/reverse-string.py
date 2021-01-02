f5dbe92d7d45ae5cca6e83f3d76f189d3b2117e5
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2021-01-02 03:22:16 -0500

    Update reverse-string.py

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -2,18 +2,14 @@
 344. Reverse String
 https://leetcode.com/problems/reverse-string/
 """
-​
+
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        if not s: return s
-        n = len(s)
-        mid = n // 2
-        left, right = 0, n - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
-            
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
