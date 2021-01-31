762b63f925ecf4e3729a81eff364e474e84bed9d
Stanley Wu <zystanley.wu@mail.utoronto.ca>
2021-01-31 10:17:39 -0500

    Update maximum-subarray.py

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -3,8 +3,8 @@ class Solution:
         global_max, prev_max = float('-inf'), 0
         for i, num in enumerate(nums):
             # calculate max sum ending at current i
-            if prev_max > 0: curr_max = prev_max + nums[i]
-            else: curr_max = nums[i]
+            if prev_max > 0: curr_max = prev_max + num
+            else: curr_max = num
             # update local, global
             global_max = max(global_max, curr_max)
             prev_max = curr_max
